4573132b8dc1332610b46d3134c59fb62b5c42d2
Michal <mpiechulek@gmail.com>
2023-08-04 10:22:55 +0200

    fixed styles

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,6 +4,7 @@ import { CanActivateFn, Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const token = window.sessionStorage.getItem('token');
+  //here we would need to decode the token and compare the expiration date
   if (token) {
     return true;
   } else {
